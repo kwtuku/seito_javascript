cf98286bce2135a53071e534a4b95836472e62d5
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-01-05 23:52:39 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const question = "ゲーム市場、最も売れたゲーム機は次の打ちどれ？";
+const answers = [
+    "スーパーファミコン", 
+    "プレイステーション2", 
+    "ニンテンドースイッチ", 
+    "ニンテンドーDS"
+];
+const correct = "ニンテンドーDS";
+
+console.log(document.getElementById("js-question"));
+
