413a4a6b207e328f5f0c1b930be870c03c9b8153
kwtuku <ttyttyttitti@gmail.com>
2021-01-09 23:53:58 +0900

    Fix typos

--- app.js
+++ app.js
@@ -11,62 +11,62 @@ const correct = "ニンテンドーDS";
 // 定数の文字列をHTMLに反映させる
 document.getElementById("js-question").textContent = question;
 
-const $buttun = document.getElementsByTagName("button");
-$buttun[0].textContent = answers[0];
-$buttun[1].textContent = answers[1];
-$buttun[2].textContent = answers[2];
-$buttun[3].textContent = answers[3];
+const $button = document.getElementsByTagName("button");
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
 
 // ボタンをクリックしたら正誤判定
-$buttun[0].addEventListener("click", evetFunc0);
+$button[0].addEventListener("click", evetFunc0);
 function evetFunc0(){
     window.alert("不正解！");
 }
 
-$buttun[1].addEventListener("click", evetFunc1);
+$button[1].addEventListener("click", evetFunc1);
 function evetFunc1(){
     window.alert("不正解！");
 }
 
-$buttun[2].addEventListener("click", evetFunc2);
+$button[2].addEventListener("click", evetFunc2);
 function evetFunc2(){
     window.alert("不正解！");
 }
 
-$buttun[3].addEventListener("click", evetFunc3);
+$button[3].addEventListener("click", evetFunc3);
 function evetFunc3(){
     window.alert("正解！");
 }
 
 
 // ボタンをクリックしたら正誤判定
-// $buttun[0].addEventListener("click", () => {
-//     if(correct === $buttun[0].textContent){
+// $button[0].addEventListener("click", () => {
+//     if(correct === $button[0].textContent){
 //         window.alert("正解！");
 //     } else {
 //         window.alert("不正解！");
 //     }
 // });
 
-// $buttun[1].addEventListener("click", () => {
-//     if(correct === $buttun[1].textContent){
+// $button[1].addEventListener("click", () => {
+//     if(correct === $button[1].textContent){
 //         window.alert("正解！");
 //     } else {
 //         window.alert("不正解！");
 //     }
 // });
 
-// $buttun[2].addEventListener("click", () => {
-//     if(correct === $buttun[2].textContent){
+// $button[2].addEventListener("click", () => {
+//     if(correct === $button[2].textContent){
 //         window.alert("正解！");
 //     } else {
 //         window.alert("不正解！");
 //     }
 // });
 
-// $buttun[3].addEventListener("click", () => {
-//     if(correct === $buttun[3].textContent){
+// $button[3].addEventListener("click", () => {
+//     if(correct === $button[3].textContent){
 //         window.alert("正解！");
 //     } else {
 //         window.alert("不正解！");
