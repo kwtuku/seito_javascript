9cf46c6f9f2dc14e4106f27e3833b0bcb3876f8e
kwtuku <ttyttyttitti@gmail.com>
2021-01-18 23:22:25 +0900

    Add: クリックしたら起こる処理

--- tab.js
+++ tab.js
@@ -5,9 +5,17 @@
     const $nav = $tab.querySelectorAll('[data-nav]');
     const $content = $tab.querySelectorAll('[data-content]');
     
+    // ページを読み込むと起こる処理
     const init = () => {
         $content[0].style.display = 'block';
     };
     init();
+
+    // クリックしたら起こる処理
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log('Clicked!', e);
+    };
+    $nav[0].addEventListener('click', (e) => handleClick(e));
 
 })();
