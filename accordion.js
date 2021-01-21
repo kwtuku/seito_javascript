83b80e46c60769f2a5e9034b347a44418abaca33
kwtuku <ttyttyttitti@gmail.com>
2021-01-21 22:19:12 +0900

    Add: aタグをクリックすると中身を表示、非表示にできるようにした

---
+++ accordion.js
@@ -0,0 +1,17 @@
+(() => {
+    const $elm = document.querySelector('#js-accordion');
+    const $trigger = $elm.getElementsByTagName('a');
+
+    $trigger[0].addEventListener('click', (e) => clickHandler(e));
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+
+        const $content = $trigger[0].nextElementSibling;
+        if($content.style.display === 'block'){
+            $content.style.display = 'none';
+        } else {
+            $content.style.display = 'block';
+        }
+    };
+})();
