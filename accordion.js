e098ee70eb6ce397a72d56f14382dc9ceef887d4
kwtuku <ttyttyttitti@gmail.com>
2021-01-21 22:57:12 +0900

    Add: コメントを追加

--- accordion.js
+++ accordion.js
@@ -1,7 +1,10 @@
 (() => {
+    // js-accordin idがついたhtmlの要素を$elmに代入
     const $elm = document.querySelector('#js-accordion');
+    // $elmのaタグの要素を$triggerに代入
     const $trigger = $elm.getElementsByTagName('a');
 
+    // $triggerがクリックされたら関数clickHandlerを実行
     const triggerLen = $trigger.length;
     let index = 0;
     while (index < triggerLen){
@@ -9,11 +12,16 @@
         index++;
     }
 
+    // 関数clickHandlerを定義
     const clickHandler = (e) => {
+        // aタグの働きを防止
         e.preventDefault();
 
+        // クリックされた要素を$targetに代入
         const $target = e.currentTarget;
+        // $targetの次の要素を$contentに代入
         const $content = $target.nextElementSibling;
+        // $contentを表示、非表示か場合分け
         if($content.style.display === 'block'){
             $content.style.display = 'none';
         } else {
