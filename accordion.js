9996aefa6143107c192b43919b75f7314f6c6f2b
kwtuku <ttyttyttitti@gmail.com>
2021-01-21 22:36:37 +0900

    Update: 0番目以外のaタグでもクリックしたら中身を表示、非表示にできるようにした

--- accordion.js
+++ accordion.js
@@ -2,12 +2,18 @@
     const $elm = document.querySelector('#js-accordion');
     const $trigger = $elm.getElementsByTagName('a');
 
-    $trigger[0].addEventListener('click', (e) => clickHandler(e));
+    const triggerLen = $trigger.length;
+    let index = 0;
+    while (index < triggerLen){
+        $trigger[index].addEventListener('click', (e) => clickHandler(e));
+        index++;
+    }
 
     const clickHandler = (e) => {
         e.preventDefault();
 
-        const $content = $trigger[0].nextElementSibling;
+        const $target = e.currentTarget;
+        const $content = $target.nextElementSibling;
         if($content.style.display === 'block'){
             $content.style.display = 'none';
         } else {
