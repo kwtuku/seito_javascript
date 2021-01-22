49a924115983024f3afb89a67dcb2f87fcf5d09a
kwtuku <ttyttyttitti@gmail.com>
2021-01-22 23:59:41 +0900

    Add: クラスを定義

--- accordion.js
+++ accordion.js
@@ -1,22 +1,52 @@
 (() => {
+    // クラスのイニシャルは大文字が慣習
+    class Accordion {
+        // クラスが呼ばれた直後に実行
+        constructor(){
+             // js-accordin idがついたhtmlの要素を$elmに代入
+            const $elm = document.querySelector('#js-accordion');
+            // $elmのaタグの要素を$triggerに代入
+            const $trigger = $elm.getElementsByTagName('a');
+            
+            // $triggerがクリックされたら関数clickHandlerを実行
+            const triggerLen = $trigger.length;
+            let index = 0;
+            while (index < triggerLen){
+                $trigger[index].addEventListener('click', (e) => clickHandler(e));
+                index++;
+            }
+        }
+
+        clickHandler(e){
+            // aタグの働きを防止
+            e.preventDefault();
+
+            // クリックされた要素を$targetに代入
+            const $target = e.currentTarget;
+            // $targetの次の要素を$contentに代入
+            const $content = $target.nextElementSibling;
+            // $contentを表示、非表示か場合分け
+            if($content.style.display === 'block'){
+                $content.style.display = 'none';
+            } else {
+                $content.style.display = 'block';
+            }
+        };
+    }
+})();
+
+/*
+(() => {
     // js-accordin idがついたhtmlの要素を$elmに代入
     const $elm = document.querySelector('#js-accordion');
     // $elmのaタグの要素を$triggerに代入
     const $trigger = $elm.getElementsByTagName('a');
 
-    // $triggerがクリックされたら関数clickHandlerを実行
-    const triggerLen = $trigger.length;
-    let index = 0;
-    while (index < triggerLen){
-        $trigger[index].addEventListener('click', (e) => clickHandler(e));
-        index++;
-    }
-
     // 関数clickHandlerを定義
     const clickHandler = (e) => {
         // aタグの働きを防止
         e.preventDefault();
-
+        
         // クリックされた要素を$targetに代入
         const $target = e.currentTarget;
         // $targetの次の要素を$contentに代入
@@ -37,3 +67,4 @@
         index++;
     }
 })();
+*/
