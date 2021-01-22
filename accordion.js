c7fc3b6263bd8a2bede76a26d7cdcf41196cb8a1
kwtuku <ttyttyttitti@gmail.com>
2021-01-23 00:24:13 +0900

    Add: インスタンスを生成

--- accordion.js
+++ accordion.js
@@ -2,21 +2,24 @@
     // クラスのイニシャルは大文字が慣習
     class Accordion {
         // クラスが呼ばれた直後に実行
-        constructor(){
+        constructor(obj){
+            console.log(obj.hookName, obj.tagName);
              // js-accordin idがついたhtmlの要素を$elmに代入
-            const $elm = document.querySelector('#js-accordion');
+            const $elm = document.querySelector(obj.hookName);
             // $elmのaタグの要素を$triggerに代入
-            const $trigger = $elm.getElementsByTagName('a');
+            const $trigger = $elm.getElementsByTagName(obj.tagName);
             
             // $triggerがクリックされたら関数clickHandlerを実行
             const triggerLen = $trigger.length;
             let index = 0;
             while (index < triggerLen){
-                $trigger[index].addEventListener('click', (e) => clickHandler(e));
+                // クラス内の関数を呼び出すときはthis.をつける
+                $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
                 index++;
             }
         }
 
+        // クラス内で関数を定義するときはconstructorと同じ書き方をする
         clickHandler(e){
             // aタグの働きを防止
             e.preventDefault();
@@ -31,8 +34,14 @@
             } else {
                 $content.style.display = 'block';
             }
-        };
+        }
     }
+
+    // インスタンスを生成
+    const fuckingAccordion = new Accordion({
+        hookName: '#js-accordion',
+        tagName: 'a'
+    });
 })();
 
 /*
